477f5d11a7525760fbca49149d9c513dd508eb4c
Jason Park <zeipar@gmail.com>
2021-01-17 18:35:49 -0800

    Added a checkbox component list.

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo page</h1>
+  <div class="home">
+    <v-list
+      flat
+    >
+      <v-list-item>
+        <template v-slot:default="{ active, }">
+          <v-list-item-action>
+            <v-checkbox
+              :input-value="active"
+              color="primary"
+            ></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Wake up</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
