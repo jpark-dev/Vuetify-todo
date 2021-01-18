1fbdcb421ee2e9383f0528b2baace276983a34eb
Jason Park <zeipar@gmail.com>
2021-01-17 18:50:08 -0800

    Conditionally rendered button color/text strikethrough if the task is checked.

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -8,17 +8,24 @@
         v-for="task in tasks"
         :key="task.id"
       >
-        <v-list-item>
-          <template v-slot:default="{ active, }">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5' : task.done }"
+        >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
-                :input-value="active"
+                :input-value="task.done"
                 color="primary"
               ></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through' : task.done }"
+              >
+                {{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -36,17 +43,26 @@
       tasks: [
         {
           id: 1,
-          title: 'Wake up'
+          title: 'Wake up',
+          done: false,
         },
         {
           id: 2,
-          title: 'Get bananas'
+          title: 'Get bananas',
+          done: false,
         },
         {
           id: 3,
-          title: 'Eat bananas'
+          title: 'Eat bananas',
+          done: false,
         },
       ]
+    }
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0];
+      task.done = !task.done;
     }
   }
 }
