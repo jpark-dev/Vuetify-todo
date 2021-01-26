2493645b3501b5f54da0f719402d214297dffafb
Jason Park <zeipar@gmail.com>
2021-01-26 12:58:57 -0800

    Further refactored TaskList into task.

--- src/components/Todo/ListTasks.vue
+++ src/components/Todo/ListTasks.vue
@@ -3,47 +3,20 @@
     class="pt-0"
     flat
   >
-    <div
+    <task
       v-for="task in $store.state.tasks"
       :key="task.id"
-    >
-      <v-list-item
-        @click="$store.commit('doneTask', task.id)"
-        :class="{ 'blue lighten-5' : task.done }"
-      >
-        <template v-slot:default>
-          <v-list-item-action>
-            <v-checkbox
-              :input-value="task.done"
-              color="primary"
-            ></v-checkbox>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title
-              :class="{ 'text-decoration-line-through' : task.done }"
-            >
-              {{ task.title }}
-            </v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-btn
-              @click.stop="$store.commit('deleteTask', task.id)"
-              icon
-            >
-              <v-icon color="primary lighten-1">mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </template>
-      </v-list-item>
-      <v-divider />
-    </div>
+      :task="task"
+    />
   </v-list>
 </template>
 
 <script>
+import Task from './Task.vue'
 export default {
-
+  components: {
+    Task,
+  }
 }
 </script>
 
