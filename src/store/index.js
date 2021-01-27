be60329f69bba1a03e2b7fa86709fb3983e49241
Jason Park <zeipar@gmail.com>
2021-01-26 22:51:37 -0800

    Made the snackbar to be destroyed when the 'close' button is clicked.

--- src/store/index.js
+++ src/store/index.js
@@ -38,6 +38,9 @@ export default new Vuex.Store({
         state.snackbar.text = text;
       }, timeout)
     },
+    hideSnackbar(state) {
+      state.snackbar.show = false;
+    },
   },
   actions: {
     addTask({ commit }, newTaskTitle) {
