7679e793958cb82933815e3121367659146fed4e
Jason Park <zeipar@gmail.com>
2021-01-30 16:28:53 -0800

    Added TaskMenu component.

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -20,12 +20,7 @@
           </v-list-item-title>
         </v-list-item-content>
         <v-list-item-action>
-          <v-btn
-            @click.stop="dialogs.delete = true"
-            icon
-          >
-            <v-icon color="primary lighten-1">mdi-delete</v-icon>
-          </v-btn>
+          <task-menu />
         </v-list-item-action>
       </template>
     </v-list-item>
@@ -50,6 +45,7 @@ export default {
   },
   components: {
     'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete.vue').default,
+    'task-menu': require('@/components/Todo/TaskMenu.vue').default,
   }
 }
 </script>
