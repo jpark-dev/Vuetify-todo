3b85021b7827835c2c2450595a7ae96b7a04ee05
Jason Park <zeipar@gmail.com>
2021-01-30 16:46:44 -0800

    Implemented deleteTask with menu. Used .call(this) to trigger Vue instance.

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -20,31 +20,18 @@
           </v-list-item-title>
         </v-list-item-content>
         <v-list-item-action>
-          <task-menu />
+          <task-menu :task="task"/>
         </v-list-item-action>
       </template>
     </v-list-item>
     <v-divider />
-    <dialog-delete
-      v-if="dialogs.delete"
-      :task="task"
-      @close="dialogs.delete = false"
-    />
   </div>
 </template>
 
 <script>
 export default {
   props: ['task'],
-  data() {
-    return {
-      dialogs: {
-        delete: false,
-      },
-    }
-  },
   components: {
-    'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete.vue').default,
     'task-menu': require('@/components/Todo/TaskMenu.vue').default,
   }
 }
--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -1,37 +1,51 @@
 <template>
-  <v-menu
-    bottom
-    left
-  >
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn
-        color="primary"
-        icon
-        v-bind="attrs"
-        v-on="on"
-      >
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </template>
+  <div>
+    <v-menu
+      bottom
+      left
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          icon
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
 
-    <v-list>
-      <v-list-item
-        v-for="(item, index) in items"
-        :key="index"
-        @click="handleClick(index)"
-      >
-        <v-list-item-icon>
-          <v-icon v-text="item.icon"></v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-menu>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="handleClick(index)"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <dialog-delete
+      v-if="dialogs.delete"
+      :task="task"
+      @close="dialogs.delete = false"
+    />
+  </div>
 </template>
 
 <script>
 export default {
+  props: ['task'],
+  components: {
+    'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete.vue').default,
+  },
   data: () => ({
+    dialogs: {
+      delete: false,
+    },
     items: [
       {
         title: 'Edit',
@@ -51,19 +65,15 @@ export default {
         title: 'Delete',
         icon: 'mdi-delete',
         click() {
-          console.log(this.title);
+          this.dialogs.delete = true;
         },
       },
     ],
   }),
   methods: {
     handleClick(index) {
-      this.items[index].click();
+      this.items[index].click.call(this);
     }
   }
 }
 </script>
-
-<style>
-
-</style>
