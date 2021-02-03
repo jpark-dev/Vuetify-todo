35c1af17bc30921eab2198d592e514b3b4b31dca
Jason Park <zeipar@gmail.com>
2021-02-02 19:03:53 -0800

    Edit now works with snackbar, and taskfield component is validated.

--- src/store/index.js
+++ src/store/index.js
@@ -55,6 +55,10 @@ export default new Vuex.Store({
       commit('deleteTask', id);
       commit('showSnackbar', 'Task deleted!');
     },
+    updateTaskTitle({ commit }, payload) {
+      commit('updateTaskTitle', payload);
+      commit('showSnackbar', 'Task updated!');
+    },
   },
   modules: {
   }
