032c811fe9b59cdc225316064b232ce2697efaf8
Jason Park <zeipar@gmail.com>
2021-02-11 21:28:22 -0800

    Installed date-fns and filtered the date accordingly.

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -19,25 +19,35 @@
             {{ task.title }}
           </v-list-item-title>
         </v-list-item-content>
+
         <v-list-item-action v-if="task.dueDate">
           <v-list-item-action-text>
             <v-icon small>mdi-calendar</v-icon>
-            {{ task.dueDate }}
+            {{ task.dueDate | niceDate }}
           </v-list-item-action-text>
-
         </v-list-item-action>
+
         <v-list-item-action>
           <task-menu :task="task"/>
         </v-list-item-action>
       </template>
+
     </v-list-item>
     <v-divider />
+
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns';
+
 export default {
   props: ['task'],
+  filters: {
+    niceDate(value) {
+      return format(new Date(value), 'MMM dd');
+    }
+  },
   components: {
     'task-menu': require('@/components/Todo/TaskMenu.vue').default,
   }
