2390a5e70bf755b442847eea925decd8a5ef3132
Jason Park <zeipar@gmail.com>
2021-02-13 18:30:42 -0800

    Implemented search feature.

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    search: null,
     tasks: [
       {
         id: 1,
@@ -31,6 +32,9 @@ export default new Vuex.Store({
     },
   },
   mutations: {
+    setSearch(state, value) {
+      state.search = value;
+    },
     addTask(state, newTaskTitle) {
       let newTask = {
         id: Date.now(),
@@ -88,6 +92,14 @@ export default new Vuex.Store({
       commit('showSnackbar', 'DueDate updated!');
     },
   },
-  modules: {
-  }
+  getters: {
+    tasksFiltered(state) {
+      if (!state.search) {
+        return state.tasks;
+      }
+      return state.tasks.filter(task =>
+        task.title.toLowerCase().includes(state.search.toLowerCase())
+      )
+    },
+  },
 })
